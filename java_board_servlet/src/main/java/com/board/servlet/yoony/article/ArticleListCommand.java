292bb9a74566c2c89a18fae5a0f3c0cfa292a73d
ykkim <code.sugartoast@gmail.com>
2023-02-16 15:19:27 +0900

    mybatis 적용 완료, 주석 강화, 예외처리 강화

--- java_board_servlet/src/main/java/com/board/servlet/yoony/article/ArticleListCommand.java
+++ java_board_servlet/src/main/java/com/board/servlet/yoony/article/ArticleListCommand.java
@@ -3,15 +3,16 @@ package com.board.servlet.yoony.article;
 import com.board.servlet.yoony.MainCommand;
 import com.board.servlet.yoony.article.page.PageDTO;
 import com.board.servlet.yoony.category.CategoryDAO;
+import com.board.servlet.yoony.category.CategoryDTO;
 import com.board.servlet.yoony.database.MyBatisConfig;
 import com.board.servlet.yoony.util.ValidationChecker;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.ibatis.session.SqlSession;
 
 /**
  * 게시글 목록을 보여주는 커맨드
@@ -22,64 +23,94 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ArticleListCommand implements MainCommand {
 
-
+  /**
+   * 게시글 목록을 가져옴
+   * categoryDAO.selectCategoryList()를 통해 카테고리 목록을 가져온 후
+   * articleDAO.selectArticleList()를 통해 게시글 목록을 가져온다.
+   * 게시글 목록을 가져올 때는 검색을 위한 param을 넘겨준다.
+   * 검색 조건은 searchWord, category, startDate, endDate, pageNum(현재 페이지) 이다.
+   * @author yoony
+   * @since 2023. 02. 16.
+   * @version 1.0
+   * @see MainCommand
+   * @see ArticleDAO#selectArticleCount(Map)
+   * @see ArticleDAO#selectArticleList(Map)
+   * @see CategoryDAO#selectCategoryList()
+   * @param request
+   *        HttpServletRequest
+   * @param response
+   *        HttpServletResponse
+   * @throws IOException
+   */
   @Override
-  public void execute(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+  public void execute(HttpServletRequest request, HttpServletResponse response) {
 
-    // 검색조건 지정을 위한 category 가져옴
-    CategoryDAO categoryDAO = CategoryDAO.getInstance();
-    // List에서는 table 출력시 category ID를 name으로 변환이 필요하기에 category Map 형태로 반환
-    Map<Integer, String> categoryMap = categoryDAO.getCategoryMap();
-    request.setAttribute("categoryMap",categoryMap);
-
-    // 검색을 위한 param 설정
-    // TODO: 새롭게 SearchManager를 만들었지만 List부분은 기존 방식을 유지하다가 나중에 변경할 예정
-    // SearchManager는 일단은 List 이외의 페이지나 요청에서 검색을 요청하기 위한 용도로 사용될 예정
-    Map<String, Object> param = new HashMap<String, Object>();
-    String searchWord = request.getParameter("searchWord");
-    String categoryId = request.getParameter("categoryId");
-    String startDate = request.getParameter("startDate");
-    String endDate = request.getParameter("endDate");
-
-    param.put("searchWord", searchWord);
-    param.put("categoryId", categoryId);
-    param.put("startDate", request.getParameter("startDate"));
-    param.put("endDate", request.getParameter("endDate"));
-
-    request.setAttribute("searchWord", searchWord);
-    request.setAttribute("categoryId", categoryId);
-    request.setAttribute("startDate", startDate);
-    request.setAttribute("endDate", endDate);
-
-
+    // MyBatis instance 가져옴
     MyBatisConfig myBatisConfig = MyBatisConfig.getInstance();
-    ArticleDAO articleDAO = myBatisConfig.getSqlSessionFactory().openSession().getMapper(ArticleDAO.class);
+    // auto close를 위한 try-with-resource
+    try (SqlSession sqlSession = myBatisConfig.getSqlSessionFactory().openSession()) {
+      // MyBatis Mapper 가져옴
+      ArticleDAO articleDAO = sqlSession.getMapper(ArticleDAO.class);
+      CategoryDAO categoryDAO = sqlSession.getMapper(CategoryDAO.class);
 
-    // 페이지네이션 구현
-    // int totalCount = articleDAO.getArticleCount(param);
-    int totalCount = articleDAO.selectArticleCount(param);
-    int pageSize = 5;
-    int blockSize = 10;
+      // categoryList를 가져와 Map 형태로 변환
+      // Mybatis에서 자체적으로 해결하면 좋지만 고민 시간이 너무 길어져서 일단 비효율적이라도 진행
+      // TODO: List => Map 변환을 거치지 않고 Mybatis 자체적으로 Map 리턴 고려
+      List<CategoryDTO> categoryList = categoryDAO.selectCategoryList();
 
-    int pageNum;
-    String pageTemp = request.getParameter("pageNum");
-    if (!ValidationChecker.CheckStringIsNullOrEmpty(pageTemp)) {
-      pageNum = Integer.parseInt(pageTemp);
-    } else {
-      pageNum = 1;
+      Map<Integer, String> categoryMap = new HashMap<>();
+      for (CategoryDTO categoryDTO : categoryList) {
+        categoryMap.put(categoryDTO.getCategoryId(), categoryDTO.getName());
+      }
+      request.setAttribute("categoryMap", categoryMap);
+
+      // 검색을 위한 param 설정
+
+
+      // 검색을 위한 param 설정
+      // TODO: 새롭게 SearchManager를 만들었지만 List부분은 기존 방식을 유지하다가 나중에 변경할 예정
+      // SearchManager는 일단은 List 이외의 페이지나 요청에서 검색을 요청하기 위한 용도로 사용될 예정
+      Map<String, Object> param = new HashMap<String, Object>();
+      String searchWord = request.getParameter("searchWord");
+      String categoryId = request.getParameter("categoryId");
+      String startDate = request.getParameter("startDate");
+      String endDate = request.getParameter("endDate");
+
+      param.put("searchWord", searchWord);
+      param.put("categoryId", categoryId);
+      param.put("startDate", request.getParameter("startDate"));
+      param.put("endDate", request.getParameter("endDate"));
+
+      request.setAttribute("searchWord", searchWord);
+      request.setAttribute("categoryId", categoryId);
+      request.setAttribute("startDate", startDate);
+      request.setAttribute("endDate", endDate);
+
+      // 페이지네이션 구현
+      int totalCount = articleDAO.selectArticleCount(param);
+      int pageSize = 5;
+      int blockSize = 10;
+
+      int pageNum;
+      String pageTemp = request.getParameter("pageNum");
+      if (!ValidationChecker.CheckStringIsNullOrEmpty(pageTemp)) {
+        pageNum = Integer.parseInt(pageTemp);
+      } else {
+        pageNum = 1;
+      }
+      PageDTO pageDTO = new PageDTO(pageNum, pageSize, blockSize, totalCount);
+      request.setAttribute("pageDTO", pageDTO);
+
+      int rowStart = (pageNum - 1) * pageSize + 1;
+      int rowEnd = rowStart + pageSize - 1;
+      param.put("rowStart", rowStart);
+      param.put("rowEnd", rowEnd);
+
+      // 검색조건에 따른 게시글 가져옴
+      List<ArticleDTO> articleList = articleDAO.selectArticleList(param);
+      request.setAttribute("articleList",articleList);
+    } catch (Exception e) {
+      e.printStackTrace();
     }
-    PageDTO pageDTO = new PageDTO(pageNum, pageSize, blockSize, totalCount);
-    request.setAttribute("pageDTO", pageDTO);
-
-    int rowStart = (pageNum - 1) * pageSize + 1;
-    int rowEnd = rowStart + pageSize - 1;
-    param.put("rowStart", rowStart);
-    param.put("rowEnd", rowEnd);
-
-    // 검색조건에 따른 게시글 가져옴
-    //List<ArticleDTO> articleList = articleDAO.getArticleList(param);
-    List<ArticleDTO> articleList = articleDAO.selectArticleList(param);
-    request.setAttribute("articleList",articleList);
   }
 }
--- java_board_servlet/src/main/java/com/board/servlet/yoony/database/MyBatisConfig.java
+++ java_board_servlet/src/main/java/com/board/servlet/yoony/database/MyBatisConfig.java
@@ -4,18 +4,44 @@ import java.io.IOException;
 import java.io.InputStream;
 import org.apache.ibatis.builder.xml.XMLConfigBuilder;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+/**
+ * MyBatis 설정 클래스
+ * @author yoony
+ * @version 1.0
+ * @since 2023. 02. 16.
+ */
 public class MyBatisConfig {
+
+
+  private Logger logger = LogManager.getLogger(MyBatisConfig.class);
 
   private static MyBatisConfig instance;
   private SqlSessionFactory sqlSessionFactory;
 
+  /**
+   * private 생성자
+   * getInstance()를 통해서만 객체 생성 가능
+   * sqlSessionFactory를 생성하는 setupSqlSessionFactory()를 호출
+   * @author yoony
+   * @version 1.0
+   * @since 2023. 02. 16.
+   * @see #getInstance()
+   * @see #setupSqlSessionFactory()
+   */
   private MyBatisConfig() {
     setupSqlSessionFactory();
   }
 
+  /**
+   * 싱글톤 패턴을 이용한 MyBatisConfig 객체 생성
+   * @return MyBatisConfig 객체
+   */
   public static MyBatisConfig getInstance() {
     if (instance == null) {
       instance = new MyBatisConfig();
@@ -26,14 +52,26 @@ public class MyBatisConfig {
   public SqlSessionFactory getSqlSessionFactory() {
     return sqlSessionFactory;
   }
-
+  /**
+   * MyBatis 설정 파일을 읽어 SqlSessionFactory를 생성하는 메소드
+   * 싱글톤 방식으로 private 생성자인 MyBatisConfig()에서 1회만 호출
+   * 생성된 sqlSessionFactory는 getSqlSessionFactory()를 통해 사용 가능
+   * @author yoony
+   * @version 1.0
+   * @since 2023. 02. 16.
+   * @see #MyBatisConfig()
+   * @see #getSqlSessionFactory()
+   *
+   */
   private void setupSqlSessionFactory() {
     try {
     InputStream configuration = Resources.getResourceAsStream("Mybatis.xml");
     sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);
     } catch (IOException e) {
+      logger.error("MyBatis 설정 파일을 읽는 중 오류가 발생했습니다. :" + e.getMessage());
       e.printStackTrace();
     } catch (Exception e) {
+      logger.error("setupSqlSessionFactory() Exception : " + e.getMessage());
       e.printStackTrace();
     }
   }
